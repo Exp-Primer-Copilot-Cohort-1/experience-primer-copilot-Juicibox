84789f81d1a65ec9f68d8e94b832d8bd9570b5e4
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-09-21 20:48:16 +0000

    other commit

--- comments.js
+++ comments.js
@@ -1,11 +1,18 @@
 // create web server
-// 1. import express
-const express = require('express')
-// 2. create express object
-const app = express()
-// 3. create router
-const router = express.Router()
-// 4. import data
-const comments = require('../data/comments')
-// 5. import uuid
-const { v4: uuidv4 } = require('uuid');
+
+// create web server for comments
+//=============
+
+// import module
+const express = require('express');
+const router = express.Router();
+const commentsController = require('../controllers/commentsController');
+
+// Handle request
+router.get('/', commentsController.getComments);
+router.get('create', commentsController.createComment);
+router.post('create', commentsController.postComment);
+router.get('delete', commentsController.deleteComment);
+
+// export router
+module.exports = router;
